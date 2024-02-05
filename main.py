c0d38dcc7c8cd373ac7e7b468a8bb23893d99d4b
Dean Phillips <dean.phillips@e-hps.com>
2024-02-04 22:40:31 -0600

    chunking

--- main.py
+++ main.py
@@ -5,10 +5,25 @@ from dotenv import load_dotenv
 #       page_content: <contents of file>
 #       metadata: ("source": "facts.txt")
 from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 
 load_dotenv()
 
-loader = TextLoader("facts.txt")
-docs = loader.load()
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    # This isn't very intuitive.  It will basically grab 200 characters (chunk_size) and then read backwards to the nearest separator and use
+    # that as a chunk.
+    chunk_size=200,
+    # this one will cause an overlap that will start the next chunk by reading backwards.  The purpose of this is to avoid awkard chunks in
+    # which context might get lost through chunking.
+    chunk_overlap=0
+)
 
-print(docs)
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(
+    text_splitter=text_splitter
+)
+
+for doc in docs:
+    print(doc.page_content)
+    print("\n")
